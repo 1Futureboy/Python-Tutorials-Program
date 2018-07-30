5d18c1e7483b504e53030ce04c649ed9110c0ed9
Aadarsh Raj(Futureboy) <39348021+1Futureboy@users.noreply.github.com>
2018-07-30 14:03:47 +0530

    Add files via upload

---
+++ automate_your browser with python.py
@@ -0,0 +1,17 @@
+#Automate Your Browser With Python
+
+
+#Importing Modules
+
+import webbrowser            #This Modules For Opeining A Browser And Automate Browser
+import pyperclip             #This Modules For Copy And pasteing A text Clip
+
+link=pyperclip.paste()       #In Link variable pyperclip.paste()Module Is Paste Any Text You Copy On Your System
+print(webbrowser.open(link)) #This print function is print webbrowser module and webbrowser.open(link) open a link who you copy and pyperclip paste the link and then webbrowser open which link you copy.
+
+
+
+
+
+
+#Thanks For Checking Our Github
